401c1f50020ddf5ceaa3e0299930dc2da8aa926e
wsx2792 <wsx2792@gmail.com>
2021-05-19 21:01:38 +0900

    18.3 FormError and Restaurant Tests

---
+++ src/components/__test__/restaurant.spec.tsx
@@ -0,0 +1,23 @@
+import { render } from "@testing-library/react";
+import React from "react";
+import { Restaurant } from "../restaurant";
+import {BrowserRouter as Router} from "react-router-dom";
+
+describe("<Restaurant />", () => {
+    it("renders OK with props", () => {
+        const restaurantProps = {
+            id: "1",
+            name:"name",
+            categoryName:"catTest",
+            coverImg:"x",
+        }
+        const {debug, getByText, container} = render(
+            <Router>
+                <Restaurant {...restaurantProps} />
+            </Router>
+        );
+        getByText(restaurantProps.name);
+        getByText(restaurantProps.categoryName);
+        expect(container.firstChild).toHaveAttribute("href", `/restaurant/${restaurantProps.id}`);
+    })
+});
