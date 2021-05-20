855b2e1e695ceaff4d6406d22160b801faac1d50
wsx2792 <wsx2792@gmail.com>
2021-05-20 22:23:15 +0900

    18.8 CreateAccount Tests part One

---
+++ src/components/__test__/create-account.spec.tsx
@@ -0,0 +1,26 @@
+import { ApolloProvider } from "@apollo/client";
+import { RenderResult } from "@testing-library/react";
+import { createMockClient, MockApolloClient } from "mock-apollo-client";
+import React from "react";
+import { CreateAccount } from "../../pages/create-account";
+import { render, waitFor } from "../../test-utils";
+
+describe("<CreateAccount/>", () => {
+    let mockedClient: MockApolloClient;
+    let renderResult: RenderResult;
+    beforeEach(async () => {
+        await waitFor(() => {
+            mockedClient = createMockClient();
+            renderResult = render(
+                <ApolloProvider client={mockedClient}>
+                    <CreateAccount/>
+                </ApolloProvider>
+            );
+        });
+    })
+    it("renders OK", async () => {
+        await waitFor(() => {
+            expect(document.title).toBe("Create Account | Nuber Eats");
+        })
+    });
+});
