b53935cbb1373d15f1d174c2794872c18cd80da0
wsx2792 <wsx2792@gmail.com>
2021-05-20 22:44:54 +0900

    18.9 CreateAccount Tests part Two

--- src/components/__test__/create-account.spec.tsx
+++ src/components/__test__/create-account.spec.tsx
@@ -1,9 +1,10 @@
 import { ApolloProvider } from "@apollo/client";
-import { RenderResult } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import { createMockClient, MockApolloClient } from "mock-apollo-client";
 import React from "react";
-import { CreateAccount } from "../../pages/create-account";
-import { render, waitFor } from "../../test-utils";
+import { CreateAccount, CREATE_ACCOUNT_MUTATION } from "../../pages/create-account";
+import { render, waitFor, RenderResult } from "../../test-utils";
+import { UserRole } from "../../__generated__/globalTypes";
 
 describe("<CreateAccount/>", () => {
     let mockedClient: MockApolloClient;
@@ -23,4 +24,63 @@ describe("<CreateAccount/>", () => {
             expect(document.title).toBe("Create Account | Nuber Eats");
         })
     });
+    it("renders validation errors", async() => {
+        const {getByRole, getByPlaceholderText} = renderResult;
+        const email = getByPlaceholderText(/email/i);
+        const password = getByPlaceholderText(/password/i);
+        const button = getByRole("button");
+        await waitFor(()=>{
+            userEvent.type(email, "wont@work");
+        });
+        let errorMessage = getByRole("alert");
+        expect(errorMessage).toHaveTextContent(/Please enter a valid email/i);
+        await waitFor(()=> {
+            userEvent.clear(email);
+        });
+        errorMessage = getByRole("alert");
+        expect(errorMessage).toHaveTextContent(/Email is required/i);
+        await waitFor(() => {
+            userEvent.type(email, "woring@email.com");
+            userEvent.click(button);
+        });
+        errorMessage = getByRole("alert");
+        expect(errorMessage).toHaveTextContent(/Password is required/i);
+    });
+    it("submits mutation with form values", async () => {
+        const {getByRole, getByPlaceholderText} = renderResult;
+        const email = getByPlaceholderText(/email/i);
+        const password = getByPlaceholderText(/password/i);
+        const button = getByRole("button");
+        const formData = {
+            email:"woring@email.com",
+            password:"123",
+            role:UserRole.Client,
+        };
+        const mockedLoginMutationResponse = jest.fn().mockResolvedValue({
+            data: {
+                createAccount: {
+                    ok: true,
+                    error:"mutation-error",
+                },
+            },
+        });
+        mockedClient.setRequestHandler(CREATE_ACCOUNT_MUTATION, mockedLoginMutationResponse);
+        jest.spyOn(window, "alert").mockImplementation(() => null);
+        await waitFor(() => {
+            userEvent.type(email, formData.email);
+            userEvent.type(password, formData.password);
+            userEvent.click(button);
+        });
+        expect(mockedLoginMutationResponse).toHaveBeenCalledTimes(1);
+        expect(mockedLoginMutationResponse).toHaveBeenCalledWith({
+            createAccountInput: {
+                email: formData.email,
+                password: formData.password,
+                role: formData.role,
+            },
+        });
+        expect(window.alert).toHaveBeenCalledWith("Account Created! Log in now!");
+        const mutationError = getByRole("alert");
+        expect(mutationError).toHaveTextContent("mutation-error");
+    })
 });
