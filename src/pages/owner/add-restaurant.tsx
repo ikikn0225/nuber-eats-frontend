14f62dfd81eb30500b183ea43ad533462242863d
wsx2792 <wsx2792@gmail.com>
2021-05-24 08:53:54 +0900

    20.4 Create Restaurant part Two

--- src/pages/owner/add-restaurant.tsx
+++ src/pages/owner/add-restaurant.tsx
@@ -1,9 +1,10 @@
 import { gql, useMutation } from "@apollo/client";
-import React from "react";
+import React, { useState } from "react";
 import { Helmet } from "react-helmet-async";
 import { useForm } from "react-hook-form";
 import { Button } from "../../components/button";
-import { createAccountMutation, createAccountMutationVariables } from "../../__generated__/createAccountMutation";
+import { FormError } from "../../components/form-error";
+import { createRestaurant, createRestaurantVariables } from "../../__generated__/createRestaurant";
 
 const CREATE_RESTAURANT_MUTATION = gql `
     mutation createRestaurant($input:CreateRestaurantInput!) {
@@ -18,31 +19,72 @@ interface IFormProps {
     name:string;
     address:string;
     categoryName:string;
+    file:FileList;
 }
 
 export const AddRestaurant = () => {
-    const [createAccountMutation, {loading, data}] = useMutation<createAccountMutation, createAccountMutationVariables>(CREATE_RESTAURANT_MUTATION);
+    const onCompleted = (data:createRestaurant) => {
+        const { createRestaurant: { ok, error } } = data;
+        if(ok)
+            setUploading(false);
+    }
+    const [createAccountMutation, {data}] = useMutation<createRestaurant, createRestaurantVariables>(CREATE_RESTAURANT_MUTATION, {
+        onCompleted,
+    });
     const {register, getValues, formState, formState: { errors }, handleSubmit } = useForm<IFormProps>({
         mode:"onChange",
-    })
-    const onSubmit = () => {
-        console.log(getValues());
+    });
+    const [ uploading, setUploading ] = useState(false);
+    const onSubmit = async() => {
+        try {
+            setUploading(true);
+            const { file, name, address, categoryName } = getValues();
+            const actualFile = file[0];
+            const formBody = new FormData();
+            formBody.append("file", actualFile);
+            const { url:coverImg }= await (
+                await fetch("http://localhost:4000/uploads/", {
+                    method:"POST",
+                    body: formBody,
+                })
+            ).json();
+            createAccountMutation({
+                variables: {
+                    input: {
+                        name,
+                        address,
+                        categoryName,
+                        coverImg,
+                    }
+                }
+            })
+        } catch(e) {
+            
+        }
+        
+
     }
     return(
-        <div className="container">
+        <div className="container flex flex-col items-center mt-52">
             <Helmet>
                 <title>Add Restaurant | Nuber Eats</title>
             </Helmet>
-            <h1>Add Restaurant</h1>
-            <form onSubmit={handleSubmit(onSubmit)}>
+            <h4 className="font-semibold text-2xl mb-3">Add Restaurant</h4>
+            <form onSubmit={handleSubmit(onSubmit)} className="grid max-w-screen-sm gap-3 mt-5 w-full mb-5">
                 <input className="input" type="text" placeholder="name" {...register("name", {required:"Name is required."})} />
                 <input className="input" type="text" placeholder="address" {...register("address", {required:"Address is required."})} />
                 <input className="input" type="text" placeholder="categoryName" {...register("categoryName", {required:"CategoryName is required."})} />
+                <div>
+                    <input type="file" accept="image/" {...register("file", {required:true})} />
+                </div>
                 <Button
-                    loading={loading}
+                    loading={uploading}
                     canClick={formState.isValid}
                     actionText="Create Restaurant"
                 />
+                {data?.createRestaurant.error && (
+                    <FormError errorMessage={data.createRestaurant.error} />
+                )}
             </form>
         </div>
     )
