a6a9b22b01810d213e398f50fb2843753fda4b56
wsx2792 <wsx2792@gmail.com>
2021-05-27 21:25:03 +0900

    0

--- src/fragment.ts
+++ src/fragment.ts
@@ -23,22 +23,3 @@ export const CATEGORY_FRAGMENT = gql `
         restaurantCount
     }
 `;
-
-export const DISH_FRAGMENT = gql`
-  fragment DishParts on Dish {
-    id
-    name
-    price
-    photo
-    description
-    options {
-      name
-      extra
-      choices {
-        name
-        extra
-      }
-    }
-  }
-`;
-
--- src/pages/owner/my-restaurant.tsx
+++ src/pages/owner/my-restaurant.tsx
@@ -2,27 +2,22 @@ import { useQuery } from "@apollo/client";
 import gql from "graphql-tag";
 import React from "react";
 import { useParams } from "react-router";
-import { Link } from "react-router-dom";
-import { RESTAURANT_FRAGMENT, DISH_FRAGMENT } from "../../fragment";
+import { RESTAURANT_FRAGMENT } from "../../fragment";
 import { myRestaurant, myRestaurantVariables } from "../../__generated__/myRestaurant";
 import { MY_RESTAURANTS_QUERY } from "./my-restaurants";
 
 
-export const MY_RESTAURANT_QUERY = gql `
+const MY_RESTAURANT_QUREY = gql `
     query myRestaurant($input:MyRestaurantInput!) {
         myRestaurant(input:$input) {
             ok
             error
             restaurant {
                 ...RestaurantParts
-                menu {
-                    ...DishParts
-                }
             }
         }
     }
     ${RESTAURANT_FRAGMENT}
-    ${DISH_FRAGMENT}
 `
 
 interface IParams {
@@ -32,7 +27,7 @@ interface IParams {
 export const MyRestaurant = () => {
     const {id} = useParams<IParams>();
     const {data} = useQuery<myRestaurant, myRestaurantVariables>(
-        MY_RESTAURANT_QUERY,
+        MY_RESTAURANTS_QUERY,
         {
             variables: {
                 input: {
@@ -41,32 +36,7 @@ export const MyRestaurant = () => {
             }
         }
         )
-console.log(data);
-
-    return (
-    <div>
-      <div
-        className="  bg-gray-700  py-28 bg-center bg-cover"
-        style={{
-          backgroundImage: `url(${data?.myRestaurant.restaurant?.coverImg})`,
-        }}
-      ></div>
-      <div className="container mt-10">
-        <h2 className="text-4xl font-medium mb-10">
-          {data?.myRestaurant.restaurant?.name || "Loading..."}
-        </h2>
-        <Link to={`/restaurant/${id}/add-dish`} className=" mr-8 text-white bg-gray-800 py-3 px-10">
-          Add Dish &rarr;
-        </Link>
-        <Link to={``} className=" text-white bg-green-700 py-3 px-10">
-          Buy Promotion &rarr;
-        </Link>
-        <div className="mt-10">
-          {data?.myRestaurant.restaurant?.menu?.length === 0 ? (
-            <h4 className="text-xl mb-5">Please upload a dish!</h4>
-          ) : null}
-        </div>
-      </div>
-    </div>
-  );
+    console.log(data);
+    
+    return <h1>My Restaurant</h1>
 }
